7ce5d647bc46aa516bf503b5cab31e6ff5512f77
Mauro Ezequiel Massa <pormauro@gmail.com>
2025-11-15 02:23:44 -0300

    feat: highlight addresses with gps coordinates

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -18,6 +18,7 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'mappin.circle.fill': 'location-on',
 } as IconMapping;
 
 /**
